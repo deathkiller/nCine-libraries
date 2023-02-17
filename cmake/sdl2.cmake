c4a1a2b5e61440ce78bacbd69c4b4a10d661a9a4
Death Killer <884052+deathkiller@users.noreply.github.com>
2023-02-17 18:18:17 +0100

    Fixed signature

--- cmake/sdl2.cmake
+++ cmake/sdl2.cmake
@@ -1,6 +1,6 @@
 set(TARGET_SDL2 sdl2)
 set(URL_SDL2 https://www.libsdl.org/release/SDL2-2.26.3.tar.gz)
-set(URL_MD5_SDL2 cf539ffe9e0dd6f943ac9de75fd2e56e)
+set(URL_MD5_SDL2 6ff7cbe9f77fc5afa79d54895dc6becc)
 set(LIBNAME_SDL2 SDL2)
 set(LIBNAME_SDL2MAIN SDL2main)
 
